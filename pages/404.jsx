2f3a99b7ee7c050ee8efad3ed75da97321ece5e2
Diome Nike Potot <diome.halcyonwebdesign@gmail.com>
2024-03-01 14:46:50 +0800

    chore: refactor 404 pages

--- pages/404.jsx
+++ pages/404.jsx
@@ -1,3 +1,20 @@
+import Link from "next/link";
 export default function Custom404() {
-  return <h1>404 - Page Not Found</h1>;
+  return (
+    <div class="bg-gray-100 flex flex-col items-center justify-center h-screen">
+      <div class="text-center">
+        <h1 class="text-[140px] mb-[15px] font-bold text-primary">404</h1>
+        <p class="text-2xl font-semibold text-gray-600 mb-4">Page Not Found</p>
+        <p class="text-lg text-gray-700 mb-8">
+          {`The page you're looking for doesn't exist.`}
+        </p>
+        <Link
+          href="/"
+          class="text-lg font-semibold text-blue-500 hover:underline"
+        >
+          Go back to homepage
+        </Link>
+      </div>
+    </div>
+  );
 }
