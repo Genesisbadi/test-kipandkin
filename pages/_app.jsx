f7cb1bbc961794ae32683d25c43e3837a7d94aea
Diome Nike Potot <diome.halcyonwebdesign@gmail.com>
2024-03-04 15:47:55 +0800

    chore: refactor nprogress loader

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -5,7 +5,6 @@ import DefaultLayout from "@/components/_layout/DefaultLayout";
 import globalState from "@/lib/store/globalState";
 import persistentStore from "@/lib/store/persistentStore";
 import { useEffect } from "react";
-import NextTopLoader from "nextjs-toploader";
 
 import { Montserrat } from "next/font/google";
 
@@ -42,21 +41,6 @@ export default function App({ Component, pageProps }) {
       <DefaultLayout>
         <Component {...pageProps} />
       </DefaultLayout>
-      <NextTopLoader
-        color="#691A31"
-        initialPosition={0.08}
-        crawlSpeed={100}
-        height={3}
-        crawl={true}
-        showSpinner={true}
-        easing="ease"
-        speed={100}
-        shadow="0 0 10px #691A31,0 0 5px #691A31"
-        template='<div class="bar" role="bar"><div class="peg"></div></div> 
-  <div class="spinner" role="spinner"><div class="spinner-icon"></div></div>'
-        zIndex={1600}
-        showAtBottom={false}
-      />
     </div>
   );
 }
