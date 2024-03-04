12c23a63eac0630aeaf15bd75d3daedb55525f1e
Diome Nike Potot <diome.halcyonwebdesign@gmail.com>
2024-03-04 11:13:08 +0800

    chore: refactor call to actions

--- components/_layout/partials/footer/FooterCallToActions.jsx
+++ components/_layout/partials/footer/FooterCallToActions.jsx
@@ -1,35 +1,42 @@
 import globalData from "@/lib/preBuildScripts/static/globalData.json";
 import Image from "next/image";
+import { useRouter } from "next/router";
 export default function CallToActions() {
+  const router = useRouter();
   const { block_title, items } = globalData.tenantDetails.data.call_to_actions;
   return (
-    <section className="text-primary py-[30px] border-y-[3px] border-primary">
-      <div className="container">
-        <h2 className="text-primary text-[22px] mb-[30px] mb-[15px]">
-          {block_title || "Hello World"}
-        </h2>
-        {items && (
-          <div className="flex flex-wrap">
-            {items?.map((item, index) => (
-              <div
-                key={index}
-                className="flex mb-[30px] md:max-w-[50%] lg:max-w-[33.33%] w-full"
-              >
-                <span className="mr-[15px] min-w-[60px] min-h-[60px] w-[60px] h-[60px] p-[5px] rounded-full flex items-center justify-center bg-primary ">
-                  <Image
-                    src={item?.icon}
-                    width={40}
-                    height={40}
-                    alt={item?.title}
-                    className="!invert-[100%] !brightness-[100%] !contrast-[100%]"
-                  />
-                </span>
-                <h3>{item?.title}</h3>
+    <>
+      {router.asPath !== "/" && (
+        <section className="text-primary py-[30px] border-y-[3px] border-primary">
+          <div className="container">
+            <h2 className="text-primary text-[22px] mb-[30px] mb-[15px]">
+              {`${block_title} at ${globalData.tenantDetails.name}` ||
+                "Hello World"}
+            </h2>
+            {items && (
+              <div className="flex flex-wrap">
+                {items?.map((item, index) => (
+                  <div
+                    key={index}
+                    className="flex mb-[30px] md:max-w-[50%] lg:max-w-[33.33%] w-full"
+                  >
+                    <span className="mr-[15px] min-w-[60px] min-h-[60px] w-[60px] h-[60px] p-[5px] rounded-full flex items-center justify-center bg-primary ">
+                      <Image
+                        src={item?.icon}
+                        width={40}
+                        height={40}
+                        alt={item?.title}
+                        className="!invert-[100%] !brightness-[100%] !contrast-[100%]"
+                      />
+                    </span>
+                    <h3>{item?.title}</h3>
+                  </div>
+                ))}
               </div>
-            ))}
+            )}
           </div>
-        )}
-      </div>
-    </section>
+        </section>
+      )}
+    </>
   );
 }
