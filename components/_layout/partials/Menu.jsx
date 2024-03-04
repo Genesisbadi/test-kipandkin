baa052fe5d8717fd292637ae2804e67ba6897cf2
Diome Nike Potot <diome.halcyonwebdesign@gmail.com>
2024-03-04 09:25:25 +0800

    refactor: MenuHeader

--- components/_layout/partials/Menu.jsx
+++ components/_layout/partials/Menu.jsx
@@ -30,7 +30,10 @@ export default function Menu() {
 
   return (
     <>
-      <header id="header">
+      <header
+        id="header"
+        className="sticky top-0 z-[100] bg-white shadow-[0_2px_2px_-2px_rgba(0,0,0,.15)]"
+      >
         <div className="flex items-center py-[10px] xl:py-0 justify-center xl:justify-between">
           <div className="xl:pl-[30px] relative z-[1]">
             <Link href="/">
