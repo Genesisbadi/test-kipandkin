ecb13756b504556f1503430771ffa4a721f57a0c
genesishalcyon <genesis.halcyondigital@gmail.com>
2024-03-04 10:24:46 +0800

    chore: update page-bannner

--- components/blocks/PageBanner.jsx
+++ components/blocks/PageBanner.jsx
@@ -1,16 +1,18 @@
 import Image from "next/image";
 
 export default function Block({ block, mediaHandler }) {
-  const { title, image } = block.main;
-  const media = "/Banner-Safe-Space-Desktop.jpg";
+  const { title } = block.main;
 
-  console.log(block.main);
+  // console.log(mediaHandler["main.image"]?.[0].conversions.desktop, "image");
   return (
     <section className="w-full">
       <div className="reltive w-full flex justify-center items-center">
         <Image
           alt={title || "#"}
-          src={media || "#"}
+          src={
+            mediaHandler["main.image"]?.[0].conversions.desktop ||
+            "/Banner-Safe-Space-Desktop.jpg"
+          }
           width={1920}
           height={1080}
           className="w-full object cover"
