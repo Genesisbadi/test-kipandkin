53ef0f386ce4c1699f7fd9c0d1d1082666068a32
genesishalcyon <genesis.halcyondigital@gmail.com>
2024-03-11 10:52:54 +0800

    chore: implement carousel gallery

--- components/blocks/Awards.jsx
+++ components/blocks/Awards.jsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 export default function Awards({ block }) {
   const { title, images } = block.main;
   return (
-    <section className="flex bg-[#f1f1f1]">
-      <div className="container mb-[50px]">
+    <section className="flex bg-[#f1f1f1] pt-[20px]">
+      <div className="container mb-[30px]">
         <h2 className="text-[20px] text-primary text-center tracking-[1px] border-b-[1px] border-[#ccc] pb-[10px] mb-[20px]">
           {title}
         </h2>
