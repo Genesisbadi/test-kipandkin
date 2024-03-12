b2bead3b5e314fef214bf4acac5b4d57a272cf12
Diome Nike Potot <diome.halcyonwebdesign@gmail.com>
2024-03-12 11:34:35 +0800

    chore: refactor

--- components/blocks/BlogBlock.jsx
+++ components/blocks/BlogBlock.jsx
@@ -9,39 +9,13 @@ import blogCategoryTaxonomies from "@/lib/preBuildScripts/static/blog-categories
 
 export default function BlogBlock({ block }) {
   const blogCategories = blogCategoryTaxonomies.blogCategoryTaxonomies;
+  const [selectedCategory, setSelectedCategory] = useState("");
   const router = useRouter();
   const [articles, setArticles] = useState([]);
   const [loading, setLoading] = useState(true);
   const [currentPage, setCurrentPage] = useState(1);
   const [hasNextPage, setHasNextPage] = useState(false);
   const [hasPrevPage, setHasPrevPage] = useState(false);
-
-  const getArticles = async (page) => {
-    setLoading(true);
-    setHasPrevPage(false);
-    setHasNextPage(false);
-
-    try {
-      const res = await axios.get(
-        process.env.NEXT_PUBLIC_TENANT_API +
-          `/api/contents/blog/entries?page[size]=1&page[number]=${currentPage}&includes=blueprintData,mediaHandler`
-      );
-      setArticles(res.data);
-      setLoading(false);
-
-      if (res.data.links.prev) {
-        setHasPrevPage(true);
-      }
-
-      if (res.data.links.next) {
-        setHasNextPage(true);
-      }
-      // window.history.pushState({}, "", `?pager=${page}`);
-    } catch (error) {
-      console.error("Error fetching articles:", error);
-      setLoading(false);
-    }
-  };
 
   const goToPreviousPage = () => {
     if (currentPage) {
@@ -55,33 +29,7 @@ export default function BlogBlock({ block }) {
     }
   };
 
-  useEffect(() => {
-    // Extract page number from query parameter in URL on initial load
-    const urlParams = new URLSearchParams(window.location.search);
-    const pageParam = urlParams.get("pager");
-    const initialPage = parseInt(pageParam) || 1;
-    setCurrentPage(initialPage);
-  }, []);
-
-  useEffect(() => {
-    getArticles(currentPage);
-  }, [currentPage]);
-
-  function sampleFunc(e) {
-    const route_url = e.target.getAttribute("route_url");
-    if (route_url) {
-      NProgress.start();
-      router
-        .push(route_url)
-        .then(() => {
-          NProgress.done();
-        })
-        .catch(() => {
-          NProgress.done();
-        });
-    }
-  }
-  function truncateHTML(html, maxLength, linkUrl) {
+  const truncateHTML = (html, maxLength, linkUrl) => {
     const tempDiv = document.createElement("div");
 
     tempDiv.innerHTML = html;
@@ -92,27 +40,106 @@ export default function BlogBlock({ block }) {
       textContent =
         textContent.slice(0, maxLength).trim() +
         "..." +
-        " <button class='text-primary read-more' route_url='" +
+        " <button class='text-primary underline hover:text-[#2c0b14] read-more' route_url='" +
         linkUrl +
-        "'>Read More</button>";
-
-      const readMoreButton = document.querySelector(".read-more");
-      if (readMoreButton) {
-        readMoreButton.addEventListener("click", sampleFunc);
-      }
+        "'>Continue Reading</button>";
     }
 
     return textContent;
-  }
+  };
 
   useEffect(() => {
-    truncateHTML();
+    const urlParams = new URLSearchParams(window.location.search);
+    const pageParam = urlParams.get("pager");
+    const initialPage = parseInt(pageParam) || 1;
+    setCurrentPage(initialPage);
   }, []);
 
+  const filterByCategory = (e) => {
+    console.log("clicked!");
+    if (e?.target?.getAttribute("id")) {
+      setSelectedCategory(e.target.getAttribute("id"));
+      setCurrentPage(1);
+      window.history.pushState(
+        {},
+        "",
+        `?category=${e.target.getAttribute("id")}`
+      );
+    }
+  };
+
+  useEffect(() => {
+    const readMore = () => {
+      setTimeout(() => {
+        const readMoreButtons = document.querySelectorAll(".read-more");
+
+        if (readMoreButtons) {
+          readMoreButtons.forEach((readMoreButton) => {
+            readMoreButton.addEventListener("click", (e) => {
+              const route_url = e.target.getAttribute("route_url");
+              if (route_url) {
+                NProgress.start();
+                router
+                  .push(route_url)
+                  .then(() => {
+                    NProgress.done();
+                  })
+                  .catch(() => {
+                    NProgress.done();
+                  });
+              }
+            });
+          });
+        }
+      }, 300);
+    };
+
+    const getArticles = async (page) => {
+      setLoading(true);
+      setHasPrevPage(false);
+      setHasNextPage(false);
+
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+      try {
+        const res = await axios.get(
+          process.env.NEXT_PUBLIC_TENANT_API +
+            `/api/contents/blog/entries?page[size]=3&page[number]=${currentPage}&includes=blueprintData,mediaHandler&filter[taxonomies][blog-category]=${selectedCategory}`
+        );
+        setArticles(res.data);
+        setLoading(false);
+
+        if (res.data.links.prev) {
+          setHasPrevPage(true);
+        }
+
+        if (res.data.links.next) {
+          setHasNextPage(true);
+        }
+
+        if (res.status === 200) {
+          readMore();
+          filterByCategory();
+        }
+        // window.history.pushState({}, "", `?pager=${page}`);
+      } catch (error) {
+        console.error("Error fetching articles:", error);
+        setLoading(false);
+      }
+    };
+
+    getArticles(currentPage);
+    truncateHTML();
+    filterByCategory();
+  }, [currentPage, router, selectedCategory]);
+
   return (
-    <section className="py-[30px]">
+    <section className="py-[30px] bg-[#F1F1F1]">
       <div className="container">
-        <div className="flex">
+        <div className="flex mx-[-15px]">
           <div className="max-w-[70%] w-full px-[15px]">
             {loading ? (
               <>
@@ -132,49 +159,76 @@ export default function BlogBlock({ block }) {
             ) : (
               <div>
                 {articles && articles.data.length > 0 ? (
-                  <div>
-                    {articles.data.map((item, index) => (
-                      <div key={index}>
-                        <h2 className="text-primary text-[25px] mb-[15px]">
-                          <Link href={item.attributes.route_url}>
-                            {item?.attributes?.title}
-                          </Link>
-                        </h2>
+                  <>
+                    {articles.data.map((item, index) => {
+                      const date = new Date(item?.attributes?.published_at);
+                      const options = {
+                        year: "numeric",
+                        month: "short",
+                        day: "numeric",
+                      };
+                      const post_date = date.toLocaleDateString(
+                        "en-US",
+                        options
+                      );
 
-                        {item?.attributes?.data?.main?.featured_image && (
-                          <Link href={item.attributes.route_url}>
-                            <Image
-                              src={item.attributes.data.main.featured_image}
-                              className="w-full mb-[15px]"
-                              width={500}
-                              height={200}
-                              alt={item?.attributes?.title}
-                            />
-                          </Link>
-                        )}
+                      return (
+                        <div className="mb-[60px]" key={index}>
+                          <h2 className="text-primary text-[25px] mb-[30px]">
+                            <Link
+                              href={item.attributes.route_url}
+                              className="hover:underline"
+                            >
+                              {item?.attributes?.title}
+                            </Link>
+                          </h2>
 
-                        {item?.attributes?.data?.main?.description && (
-                          <>
-                            <div>
-                              <span className="text-[15px] mt-[15px]]">
-                                <div
-                                  dangerouslySetInnerHTML={{
-                                    __html: truncateHTML(
-                                      item?.attributes?.data?.main?.description,
-                                      100,
-                                      item.attributes.route_url
-                                    ),
-                                  }}
-                                />
-                              </span>
-                            </div>
-                          </>
-                        )}
-                      </div>
-                    ))}
-                  </div>
+                          {item?.attributes?.data?.main?.featured_image && (
+                            <Link href={item.attributes.route_url}>
+                              <Image
+                                src={item.attributes.data.main.featured_image}
+                                className="w-full mb-[15px]"
+                                width={500}
+                                height={200}
+                                alt={item?.attributes?.title}
+                              />
+                            </Link>
+                          )}
+
+                          {item?.attributes?.data?.main?.description && (
+                            <>
+                              <div>
+                                <span className="text-[15px] mt-[15px]]">
+                                  <div
+                                    dangerouslySetInnerHTML={{
+                                      __html: truncateHTML(
+                                        item?.attributes?.data?.main
+                                          ?.description,
+                                        100,
+                                        item.attributes.route_url
+                                      ),
+                                    }}
+                                  />
+                                </span>
+                              </div>
+                            </>
+                          )}
+
+                          {post_date && (
+                            <time
+                              className="text-[#aaa] text-[14px] mt-[20px] block mb-[10px]"
+                              dateTime={post_date}
+                            >
+                              {post_date}
+                            </time>
+                          )}
+                          <span className="h-[1px] w-[30px] bg-[#aaa] block"></span>
+                        </div>
+                      );
+                    })}
+                  </>
                 ) : (
-                  <>No results </>
+                  <>No results to show at the moment. Please try again later.</>
                 )}
                 <div className="mx-[-5px] mt-[30px] flex flex-wrap">
                   {hasPrevPage && (
@@ -201,14 +255,25 @@ export default function BlogBlock({ block }) {
               </div>
             )}
           </div>
-          <div className="w-full max-w-[30%]">
+          <div className="w-full max-w-[30%] px-[15px]">
             {blogCategories.taxonomyTerms &&
               blogCategories.taxonomyTerms.length > 0 && (
-                <>
-                  {blogCategories.taxonomyTerms.map((item, index) => (
-                    <div key={index}>{item.name}</div>
-                  ))}
-                </>
+                <div className="bg-[#E2E2E2] p-[50px]">
+                  <ul className="pl-[15px]">
+                    {blogCategories.taxonomyTerms.map((item, index) => (
+                      <li
+                        className={`cursor-pointer hover:text-primary mb-[15px] border-b-[#ccc] border-b-[1px] pb-[10px] text-[14px] ${
+                          selectedCategory === item.id ? "font-bold" : ""
+                        }`}
+                        onClick={filterByCategory}
+                        key={index}
+                        id={item.id}
+                      >
+                        {item.name}
+                      </li>
+                    ))}
+                  </ul>
+                </div>
               )}
           </div>
         </div>
