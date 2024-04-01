9123c68a3126d3418dc9e42ba62394a7b0dc5583
Diome Nike <diomenikep@gmail.com>
2024-04-01 13:56:18 +0800

    chore: refactor

--- components/blocks/Slider.jsx
+++ components/blocks/Slider.jsx
@@ -104,6 +104,7 @@ export default function Slider({ block, mediaHandler }) {
                 width={1920}
                 height={750}
                 className="absolute z-[-1] top-0 left-0 h-full w-full object-cover"
+                loading="eager"
               />
             </picture>
 
