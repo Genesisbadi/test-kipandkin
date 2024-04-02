f010b657809fd59ef788cd1722bb2811eed434b3
Diome Nike <diomenikep@gmail.com>
2024-04-02 23:53:45 +0800

    chore: refactor tenantScripts

--- components/_layout/partials/TenantScripts.jsx
+++ components/_layout/partials/TenantScripts.jsx
@@ -2,15 +2,19 @@ import tenantScripts from "@/lib/preBuildScripts/static/tenantScripts.json";
 export default function TenantScripts() {
   return (
     <>
-      {tenantScripts.map((item, index) => {
-        return (
-          <div
-            key={index}
-            id={`tenantScript-${index + 1}`}
-            dangerouslySetInnerHTML={{ __html: item.code }}
-          />
-        );
-      })}
+      {tenantScripts && tenantScripts.length > 0 && (
+        <>
+          {tenantScripts?.map((item, index) => {
+            return (
+              <div
+                key={index}
+                id={`tenantScript-${index + 1}`}
+                dangerouslySetInnerHTML={{ __html: item.code }}
+              />
+            );
+          })}
+        </>
+      )}
     </>
   );
 }
