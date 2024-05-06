8e54f441e1c260a6385ce6417831d7e2348c537c
geraldhalcyon <geraldsayson.halcyondigital@gmail.com>
2024-05-06 11:05:46 +0800

    chore: refactor

--- components/blocks/Awards.jsx
+++ components/blocks/Awards.jsx
@@ -8,15 +8,15 @@ export default function Awards({ block }) {
         <h2 className="text-[20px] text-primary text-center tracking-[1px] border-b-[1px] border-[#ccc] pb-[10px] mb-[20px]">
           {title}
         </h2>
-        <div className="flex justify-center flex-wrap">
+        <div className="flex justify-center items-center flex-wrap">
           {images?.map((item, index) => (
-            <div key={index} className="m-[15px]">
+            <div key={index} className="m-[15px] max-w-[120px] w-full">
               <Image
                 alt={title || "#"}
                 src={item}
                 width={140}
                 height={139}
-                className="w-full min-h-[100px] max-h-[139px] object-cover"
+                className="w-full object-cover mix-blend-multiply"
               />
             </div>
           ))}
