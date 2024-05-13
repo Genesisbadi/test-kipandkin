dc225e22fd71dac9a3e78f4240d9c7dd1a3c5a14
Diome Nike <diomenikep@gmail.com>
2024-05-13 13:34:19 +0800

    chore: refactor cta

--- components/partials/collapsibles/SectionAccordion.jsx
+++ components/partials/collapsibles/SectionAccordion.jsx
@@ -1,6 +1,8 @@
 import dynamic from "next/dynamic";
 import { useState } from "react";
+import globalState from "@/lib/store/globalState";
 export default function SectionAccordion({ children, ...props }) {
+  const showLazy = globalState((state) => state.showLazy);
   const [isCollapsed, setIsCollapsed] = useState(
     process.env.NEXT_PUBLIC_TEMPLATE == 1 ? false : true
   );
@@ -14,18 +16,26 @@ export default function SectionAccordion({ children, ...props }) {
   return (
     <div className={`accordion w-full ${className ? className : ""}`}>
       <div className="h-[1px] md:hidden bg-[#ccc] w-[calc(100%-30px)] mx-auto"></div>
-      {title && (
-        <div
-          className={`${
-            process.env.NEXT_PUBLIC_TEMPLATE == 1 ? "font-tenor" : "font-domine"
-          } text-primary flex justify-between items-center md:hidden py-[20px] bg-white px-[15px]`}
-          onClick={handleCollapse}
-        >
-          {title}
-          <span className={`${!isCollapsed ? "rotate-180" : ""}`}>
-            <ArrowDown className="fill-primary" />
-          </span>
-        </div>
+      {showLazy ? (
+        <>
+          {title && (
+            <div
+              className={`${
+                process.env.NEXT_PUBLIC_TEMPLATE == 1
+                  ? "font-tenor"
+                  : "font-domine"
+              } text-primary flex justify-between items-center md:hidden py-[20px] bg-white px-[15px]`}
+              onClick={handleCollapse}
+            >
+              {title}
+              <span className={`${!isCollapsed ? "rotate-180" : ""}`}>
+                <ArrowDown className="fill-primary" />
+              </span>
+            </div>
+          )}
+        </>
+      ) : (
+        <div className="h-[24px] animate-pulse bg-[#ccc] w-full mt-2 max-w-[calc(100%-15px)] block mx-auto mb-[30px]" />
       )}
       <div
         className={`${
