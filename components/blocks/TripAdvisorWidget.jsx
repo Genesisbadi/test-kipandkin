9cad90bb42a2a98d46c5d6731ee230b4e55a1c14
Diome Nike <diomenikep@gmail.com>
2024-06-10 11:38:00 +0800

    chore: refactor trip advisor

--- components/blocks/TripAdvisorWidget.jsx
+++ components/blocks/TripAdvisorWidget.jsx
@@ -1,25 +1,25 @@
 import { useEffect, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
+import Script from "next/script";
 
 export default function TripAdvisorWidget({ block }) {
-  useEffect(() => {
-    const script = document.createElement("script");
-    script.src =
-      "https://www.jscache.com/wejs?wtype=selfserveprop&uniq=546&locationId=" +
-      block?.main.location_id +
-      "&lang=en_US&rating=true&nreviews=5&writereviewlink=true&popIdx=true&iswide=false&border=true&display_version=2";
-    script.async = true;
-    document.head.appendChild(script);
-  }, []);
+  const scriptUrl =
+    "https://www.jscache.com/wejs?wtype=selfserveprop&uniq=546&locationId=" +
+    block?.main.location_id +
+    "&lang=en_US&rating=true&nreviews=5&writereviewlink=true&popIdx=true&iswide=false&border=true&display_version=2";
 
   return (
     <section className="bg-[#F1F1F1] py-[30px]">
       <div className="container">
-        <div id="TA_selfserveprop546" className="TA_selfserveprop">
-          <ul id="VvhCFP8xz" className="TA_links JZ5CKDSmc"></ul>
-        </div>
+        {scriptUrl && (
+          <div id="TA_selfserveprop546" className="TA_selfserveprop">
+            <ul id="VvhCFP8xz" className="TA_links JZ5CKDSmc"></ul>
+          </div>
+        )}
       </div>
+
+      <Script src={scriptUrl} />
     </section>
   );
 }
