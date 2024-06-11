94d5f842ae50aeec0b18b10eba02e3f119e61b36
Diome Nike <diomenikep@gmail.com>
2024-06-11 19:03:41 +0800

    chore: refactor path

--- components/blocks/GuestFeedbackBlock.jsx
+++ components/blocks/GuestFeedbackBlock.jsx
@@ -1,23 +1,22 @@
 import dynamic from "next/dynamic";
-import GuestFeedbackForm from "../partials/forms/GuestFeedbackForm";
 export default function GuestFeedbackBlock({ block }) {
-  // const GuestFeedBackForm = dynamic(() =>
-  //   import("@/components/partials/forms/GuestFeedbackForm").then(
-  //     (module) => module.default
-  //   )
-  // );
+  const GuestFeedBackForm = dynamic(() =>
+    import("@/components/partials/forms/GuestFeedbackForm").then(
+      (module) => module.default
+    )
+  );
 
   return (
-    <div className="bg-[#F1F1F1] py-[20px]">
+    <div className="bg-[#F1F1F1] py-[50px]">
       <div className="container">
-        <div className="bg-white p-[50px]">
+        <div className="bg-white [">
           {block?.main?.description && (
             <div
-              className="mb-[30px] text-[14px]"
+              className="mb-[15px] text-[14px]"
               dangerouslySetInnerHTML={{ __html: block?.main?.description }}
             />
           )}
-          <GuestFeedbackForm form={block?.main?.form} />
+          <GuestFeedBackForm form={block?.main?.form} />
         </div>
       </div>
     </div>
