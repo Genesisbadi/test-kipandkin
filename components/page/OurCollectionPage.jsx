94f6959a16117c383abe8b16cb4146dfdfd0c08d
Diome Nike <diomenikep@gmail.com>
2024-07-02 15:03:44 +0800

    chore: optimization

--- components/page/OurCollectionPage.jsx
+++ components/page/OurCollectionPage.jsx
@@ -155,18 +155,17 @@ export default function OurCollectionPage({ page }) {
       },
     ],
   };
+  
+
   return (
     <>
       <section className="page-banner relative flex items-center justify-center min-h-[560px] sm:min-h-full sm:pb-[42.2916666667%] w-full bg-[#f1f1f1]">
         <span className="absolute h-full w-full top-0 left-0 bg-[#000] opacity-[.3] z-[1]"></span>
-        <Image
-          alt={"Banner"}
-          src={inner_banner || "../images/image_makati-large.jpg"}
-          width={1920}
-          height={1080}
-          className="w-full h-full  object-cover absolute top-0 left-0"
-          priority={true}
-        />
+        <picture>
+          <source srcSet={mobile_banner} media="(max-width: 414px)" />
+          <source srcSet={inner_banner} media="(min-width: 415px)" />
+          <Image src={inner_banner} alt={title} width={1920} height={1080} className="w-full h-full object-cover absolute top-0 left-0" priority={true} />
+        </picture>
         {title && (
           <div
             className={`relative sm:absolute sm:top-[50%] sm:translate-y-[-50%] sm:absolute sm:top-[50%] sm:translate-y-[-50%] font-tenor text-[35px] md:text-[42px]  px-5 text-center text-white  z-[3] leading-[50px]`}
