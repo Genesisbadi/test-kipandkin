74c643a5ca29df9967cdd4ecd38043be8e733299
Diome Nike <diomenikep@gmail.com>
2024-07-03 19:19:28 +0800

    chore: optimization footerCalltoACtions

--- components/_layout/partials/footer/FooterCallToActions.jsx
+++ components/_layout/partials/footer/FooterCallToActions.jsx
@@ -1,38 +1,51 @@
-import globalData from "@/lib/preBuildScripts/static/globalData.json";
+
 import globalState from "@/lib/store/globalState";
-import Image from "next/image";
+
 import { useRouter } from "next/router";
+import dynamic from "next/dynamic";
+import tenantDetailsCallToActions from "@/lib/preBuildScripts/static/tenantDetailsCallToActions.json";
 export default function CallToActions() {
-  const router = useRouter();
-  const { block_title, items } = globalData.tenantDetails.data.call_to_actions;
+  const router = useRouter(); 
+ 
   const showLazy = globalState((state) => state.showLazy);
+
+  if(!showLazy) {
+    return(
+      <>
+        <div className="py-[30px] border-y-[3px] border-primary">
+          <div className="container">
+            <div className="w-full max-w-[350px] bg-[#ccc] h-[30px] mb-[30px]" />
+            <div className="flex flex-wrap">  
+              {Array.from({ length: 2 }, (_, index) => (
+                <div key={index} className="w-full flex flex-wrap items-center">
+                  {Array.from({ length: 3 }, (_, index) => (
+                    <div key={index} className="w-full flex gap-x-[15px] md:max-w-[33.33%] mb-[15px]">
+                      <div className="bg-[#ccc] rounded-full min-w-[80px] min-h-[80px]" />
+                      <div className="w-full flex flex-col justify-center">
+                        <div className="w-full bg-[#ccc] max-w-[70%] h-[10px] mb-[10px]" />
+                        <div className="w-full bg-[#ccc] max-w-[50%] h-[10px]" />
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </>
+    )
+  }
+  const Image = dynamic(() =>
+    import("next/image").then((module) => module.default)
+  );
+  const { block_title, items } = tenantDetailsCallToActions;
+
   return (
     <>
       {router.asPath !== "/" && (
         <section className="text-primary py-[30px] border-y-[3px] border-primary">
           <div className="container">
-            {!showLazy ? (
-              <>
-                <div className="w-full max-w-[350px] bg-[#ccc] h-[30px] mb-[30px]" />
-                <div className="flex flex-wrap">  
-                  {Array.from({ length: 2 }, (_, index) => (
-                    <div key={index} className="w-full flex flex-wrap items-center">
-                      {Array.from({ length: 3 }, (_, index) => (
-                        <div key={index} className="w-full flex gap-x-[15px] md:max-w-[33.33%] mb-[15px]">
-                          <div className="bg-[#ccc] rounded-full min-w-[80px] min-h-[80px]" />
-                          <div className="w-full flex flex-col justify-center">
-                            <div className="w-full bg-[#ccc] max-w-[70%] h-[10px] mb-[10px]" />
-                            <div className="w-full bg-[#ccc] max-w-[50%] h-[10px]" />
-                          </div>
-                        </div>
-                      ))}
-                    </div>
-                  ))}
-                </div>
-              </>
-            ) : (
-              <>
-                <h2
+            <h2
               className={`tracking-[1px] text-primary text-[22px] mb-[30px] mb-[15px] ${
                 process.env.NEXT_PUBLIC_TEMPLATE == 1
                   ? "font-tenor"
@@ -74,8 +87,6 @@ export default function CallToActions() {
                   </div>
                 ))}
               </div>
-            )}
-              </>
             )}
           </div>
         </section>
