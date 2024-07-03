f204c1071613836f6f79d17921e85f2ef7f8d233
Diome Nike <diomenikep@gmail.com>
2024-07-03 18:54:09 +0800

    chore: optimization footer reviews

--- components/_layout/partials/footer/FooterReviews.jsx
+++ components/_layout/partials/footer/FooterReviews.jsx
@@ -1,14 +1,31 @@
 import reviewsData from "../../../../lib/preBuildScripts/static/reviews.json";
-import "slick-carousel/slick/slick.css";
 import { useRouter } from "next/router";
 import dynamic from "next/dynamic";
 import Link from "next/link";
 import globalState from "@/lib/store/globalState";
 export default function FooterReviews() {
   const showLazy = globalState((state) => state.showLazy);
-  const Slick = dynamic(() =>
-    import("react-slick").then((module) => module.default)
-  );
+
+  if(!showLazy) {
+    return (
+      <div className="container pt-[40px]">
+        <div className="px-[70px]">
+            <div className="animate-pulse w-full max-w-[200px] h-[20px] mx-auto bg-[#ccc] mb-[40px]" />
+            <div className="animate-pulse w-full max-w-[50%] h-[20px] bg-[#ccc] mb-[20px]" />
+            <div className="animate-pulse w-full h-[10px] bg-[#ccc] mb-[10px]" />
+            <div className="animate-pulse w-full h-[10px] bg-[#ccc] mb-[10px]" />
+            <div className="animate-pulse w-full h-[10px] max-w-[50%] bg-[#ccc] mb-[20px]" />
+            <div className="animate-pulse w-full h-[10px] max-w-[90%] bg-[#ccc] mb-[10px]" />
+            <div className="animate-pulse w-full h-[10px] max-w-[70%] bg-[#ccc] mb-[10px]" />
+            <div className="animate-pulse w-full h-[10px] max-w-[50%] bg-[#ccc] mb-[10px]" />
+          </div>
+      </div>
+    )
+  }else {
+    import("slick-carousel/slick/slick.css"); 
+  }
+
+  const Slick = dynamic(() => import("react-slick").then((module) => module.default));
   const Star = dynamic(() =>
     import("@/components/icons/Star").then((module) => module.default)
   );
@@ -80,8 +97,6 @@ export default function FooterReviews() {
     ],
   };
 
-  console.log('showLazy', showLazy)
-
   return (
     <>
       {reviewsData && reviewsData.length > 0 && (
@@ -89,85 +104,70 @@ export default function FooterReviews() {
           {router.asPath !== "/" && (
             <section className="pt-[40px] footer-reviews">
               <div className="container">
-                {!showLazy ? (
+                <h2
+                  className={`text-center text-primary text-[25px] mb-[30px] tracking-[1px] ${
+                    process.env.NEXT_PUBLIC_TEMPLATE == 1
+                      ? "font-tenor"
+                      : " "
+                  }`}
+                >
+                  Reviews
+                </h2>
+                {reviewsData && (
                   <>
-                    <div className="px-[70px]">
-                    <div className="animate-pulse w-full max-w-[200px] h-[20px] mx-auto bg-[#ccc] mb-[40px]" />
-                    <div className="animate-pulse w-full max-w-[50%] h-[20px] bg-[#ccc] mb-[20px]" />
-                    <div className="animate-pulse w-full h-[10px] bg-[#ccc] mb-[10px]" />
-                    <div className="animate-pulse w-full h-[10px] bg-[#ccc] mb-[10px]" />
-                    <div className="animate-pulse w-full h-[10px] max-w-[50%] bg-[#ccc] mb-[20px]" />
-                    <div className="animate-pulse w-full h-[10px] max-w-[90%] bg-[#ccc] mb-[10px]" />
-                    <div className="animate-pulse w-full h-[10px] max-w-[70%] bg-[#ccc] mb-[10px]" />
-                    <div className="animate-pulse w-full h-[10px] max-w-[50%] bg-[#ccc] mb-[10px]" />
-                  </div>
-                  </>
-                ) : (
-                  <>
-                    <h2
-                      className={`text-center text-primary text-[25px] mb-[30px] tracking-[1px] ${
-                        process.env.NEXT_PUBLIC_TEMPLATE == 1
-                          ? "font-tenor"
-                          : " "
-                      }`}
-                    >
-                      Reviews
-                    </h2>
-                    {reviewsData && (
-                      <>
-                        <Slick className="lg:px-[70px] text-[14px]" {...settings}>
-                          {reviewsData.map((item, index) => (
-                            <div key={index}>
-                              <h3 className="font-bold text-[14px] uppercase mb-[15px]">
-                                {item.title}
-                              </h3>
+                    <Slick className="lg:px-[70px] text-[14px]" {...settings}>
+                      {reviewsData.map((item, index) => (
+                        <div key={index}>
+                          <h3 className="font-bold text-[14px] uppercase mb-[15px]">
+                            {item.title}
+                          </h3>
 
-                              {item.data.main.description && (
-                                <div
-                                  className="text-justify"
-                                  dangerouslySetInnerHTML={{
-                                    __html: item.data.main.description,
-                                  }}
-                                />
-                              )}
+                          {item.data.main.description && (
+                            <div
+                              className="text-justify"
+                              dangerouslySetInnerHTML={{
+                                __html: item.data.main.description,
+                              }}
+                            />
+                          )}
 
-                              <div className="flex flex-wrap mt-[30px]">
-                                {item.data.main.name && (
-                                  <span>{item.data.main.name},</span>
-                                )}
+                          <div className="flex flex-wrap mt-[30px]">
+                            {item.data.main.name && (
+                              <span>{item.data.main.name},</span>
+                            )}
 
-                                <span>
-                                  <Link
-                                    className="text-primary ml-[5px] underline font-bold"
-                                    href="/tripadvisor-reviews"
-                                    target="_blank"
-                                  >
-                                    {item.data.main.link_label || "View Review"}
-                                  </Link>
-                                </span>
-                              </div>
-                              {item.data.main.stars && (
-                                <div className="flex mt-[30px]">
-                                  {Array.from(
-                                    { length: parseInt(item.data.main.stars) },
-                                    (_, index) => (
-                                      <Star
-                                        width={20}
-                                        height={20}
-                                        key={index}
-                                        color="#e6b886"
-                                      />
-                                    )
-                                  )}
-                                </div>
+                            <span>
+                              <Link
+                                className="text-primary ml-[5px] underline font-bold"
+                                href="/tripadvisor-reviews"
+                                target="_blank"
+                              >
+                                {item.data.main.link_label || "View Review"}
+                              </Link>
+                            </span>
+                          </div>
+                          {item.data.main.stars && (
+                            <div className="flex mt-[30px]">
+                              {Array.from(
+                                { length: parseInt(item.data.main.stars) },
+                                (_, index) => (
+                                  <Star
+                                    width={20}
+                                    height={20}
+                                    key={index}
+                                    color="#e6b886"
+                                  />
+                                )
                               )}
                             </div>
-                          ))}
-                        </Slick>
-                      </>
-                    )}
+                          )}
+                        </div>
+                      ))}
+                    </Slick>
                   </>
                 )}
+                  
+                
               </div>
             </section>
           )}
