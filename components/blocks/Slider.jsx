16366e04ecde42753a4e630899104320e1c143df
Diome Nike <diomenikep@gmail.com>
2024-07-22 11:52:47 +0800

    chore: refactor

--- components/blocks/Slider.jsx
+++ components/blocks/Slider.jsx
@@ -189,6 +189,7 @@ export default function Slider({ block, mediaHandler }) {
                     <Link
                       className="text-[14px] uppercase border px-[30px] py-[10px] inline-block border-[1px] border-[#fff] hover:text-primary hover:bg-[#fff] transition-all duration-300 ease-in-out "
                       href={item?.url}
+                      prefetch={false}
                     >
                       {process.env.NEXT_PUBLIC_TEMPLATE == 1
                         ? "Discover More"
