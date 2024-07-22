f57b1cde89b6f53742b7ff20afd38ef8af108f9e
Diome Nike <diomenikep@gmail.com>
2024-07-22 16:00:12 +0800

    chore: updates

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,11 +2,21 @@ import ParentBlock from "@/components/page/ParentBlock";
 import { props } from "@/lib/props/page";
 export const getStaticProps = props;
 export default function Homepage({ page, blocks }) {
+  const descriptionElement = (
+    <>
+      {page?.metaData?.description && (
+        <p hidden className="hidden opacity-0 invisible">
+          {page?.metaData?.description}
+        </p>
+      )}
+    </>
+  );
   return (
     <>
       <h1 hidden className="hidden opacity-0 invisible">
         {page.metaData.title || page.name}
       </h1>
+      {descriptionElement}
       <ParentBlock page={page} blocks={blocks} />
     </>
   );
