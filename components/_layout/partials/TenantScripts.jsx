030faf9f79237c89c7ff5c90c228d7d07f192b74
Diome Nike <diomenikep@gmail.com>
2024-07-24 12:12:16 +0800

    chore: refactor third party scripts

--- components/_layout/partials/TenantScripts.jsx
+++ components/_layout/partials/TenantScripts.jsx
@@ -1,20 +1,59 @@
 import tenantScripts from "@/lib/preBuildScripts/static/tenantScripts.json";
+import Head from "next/head";
+import { useEffect, useState } from "react";
+
 export default function TenantScripts() {
+  const [scripts, setScripts] = useState([]);
+
+  const tags = tenantScripts;
+
+  useEffect(() => {
+    const extractScripts = (htmlString) => {
+      const scriptRegex = /<script\b([^>]*)>([\s\S]*?)<\/script>/gm;
+      let match;
+      while ((match = scriptRegex.exec(htmlString)) !== null) {
+        const scriptTag = match[0];
+        const scriptAttrs = match[1];
+        const scriptContent = match[2];
+
+        const srcMatch = scriptAttrs.match(/src=["']([^"']+)["']/);
+        const scriptSrc = srcMatch ? srcMatch[1] : null;
+        setScripts((prevScripts) => {
+          return [...prevScripts, { src: scriptSrc, content: scriptContent }];
+        });
+      }
+    };
+
+    tags.map((item) => {
+      extractScripts(item?.code);
+    });
+  }, [tags]);
+
+  const uniqueArray = scripts.filter((value, index) => {
+    const _value = JSON.stringify(value);
+    return (
+      index ===
+      scripts.findIndex((obj) => {
+        return JSON.stringify(obj) === _value;
+      })
+    );
+  });
+
   return (
     <>
-      {tenantScripts && tenantScripts.length > 0 && (
+      <Head>
         <>
-          {tenantScripts?.map((item, index) => {
-            return (
-              <div
-                key={index}
-                id={`tenantScript-${index + 1}`}
-                dangerouslySetInnerHTML={{ __html: item?.code }}
-              />
-            );
-          })}
+          {uniqueArray.map((script, index) => (
+            <script
+              async
+              className="third-party"
+              key={index}
+              src={script.src || ""}
+              dangerouslySetInnerHTML={{ __html: script.content }}
+            ></script>
+          ))}
         </>
-      )}
+      </Head>
     </>
   );
 }
--- pages/_app.jsx
+++ pages/_app.jsx
@@ -22,7 +22,7 @@ export default function App({ Component, pageProps }) {
         </DefaultLayout>
       </div>
 
-      {showLazy && <TenantScripts />}
+      <TenantScripts />
     </>
   );
 }
