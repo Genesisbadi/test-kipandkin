16f5111bd5bda4ce8ac0dd97032dd959f5a13c5e
Diome Nike <diomenikep@gmail.com>
2024-07-31 11:27:42 +0800

    feat: added feature2

--- components/blocks/CardOverlay.jsx
+++ components/blocks/CardOverlay.jsx
@@ -24,40 +24,42 @@ export default function Feature({ block }) {
       childrenClassname="pb-0 bg-[#F1F1F1] text-white"
     >
       <section className={`container card-overlay py-[30px]`}>
-        <div className="relative rounded-[10px] shadow-md overflow-hidden">
+        <div className="relative rounded-[10px]  shadow-md overflow-hidden">
           <Image
             src={image}
             width={900}
             height={500}
             className="absolute top-0 left-0 w-full h-full object-cover"
           />
-          <div className="relative min-h-[400px] bg-[#000] p-[30px] bg-opacity-80">
+          <div className="relative flex flex-col min-h-[350px] bg-[#000] p-[30px] bg-opacity-70">
             <h2 className=" text-[35px] font-tenor">{title}</h2>
             <div
-              className="text-shadow-md text-[15px] leading-[28px] mt-[20px]"
+              className="text-shadow-md grow text-[15px] leading-[28px] mt-[20px]"
               dangerouslySetInnerHTML={{ __html: description }}
             />
 
-            <Link
-              href={link}
-              className="font-bold gap-x-[5px] border border-white rounded-[8px] hover:bg-[#fff] hover:bg-opacity-50 transition inline-flex items-center py-[10px] px-[20px] mt-[20px] text-white"
-            >
-              {button_label ? button_label : "Discovery More"}
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="w-5 h-5"
+            <div>
+              <Link
+                href={link}
+                className="font-bold gap-x-[5px] border border-white rounded-[8px] hover:bg-[#fff] hover:bg-opacity-50 transition inline-flex items-center py-[10px] px-[20px] mt-[20px] text-white"
               >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5"
-                />
-              </svg>
-            </Link>
+                {button_label ? button_label : "Discovery More"}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-5 h-5"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5"
+                  />
+                </svg>
+              </Link>
+            </div>
           </div>
         </div>
       </section>
---
+++ components/blocks/Feature2.jsx
@@ -0,0 +1,46 @@
+import Image from "next/image";
+import Link from "next/link";
+export default function Block({ block }) {
+  const { description, link, title, image, button_label, bg_color, position } =
+    block.main;
+  return (
+    <section
+      className={`p-[50px] border-b-[2px] border-[rgba(145,145,145,0.2)] ${
+        bg_color == "white" ? "bg-white" : "bg-[#F1F1F1]"
+      }`}
+    >
+      <div className="container overflow-hidden">
+        <div
+          className={`flex flex-wrap items-center mx-[-30px] ${
+            position == "right_content" ? "flex-row-reverse" : ""
+          }`}
+        >
+          <div className="w-full md:max-w-[50%] px-[30px]">
+            <Image
+              src={image}
+              alt="Title"
+              width={600}
+              height={600}
+              className="min-h-[350px] object-cover rounded-lg"
+            />
+          </div>
+          <div className="w-full md:max-w-[50%] px-[30px] py-[30px]">
+            <h2 className="font-tenor mb-[30px] text-[23px] text-primary drop-shadow-md">
+              {title}
+            </h2>
+            <div dangerouslySetInnerHTML={{ __html: description }} />
+
+            {link && (
+              <Link
+                href={link}
+                className="bg-primary hover:opacity-70 uppercase text-[14px] shadow-lg text-white py-[10px] rounded-[5px] min-w-[150px] text-center mt-3 px-[15px] inline-block"
+              >
+                {button_label ? button_label : "Discovery More"}
+              </Link>
+            )}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
