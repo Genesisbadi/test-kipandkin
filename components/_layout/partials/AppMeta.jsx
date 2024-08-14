8e42a3995783c5f6b757195d26b121fa484ed999
Diome Nike Potot <diomenikep@gmail.com>
2024-08-14 10:09:42 +0800

    chore: refactor

--- components/_layout/partials/AppMeta.jsx
+++ components/_layout/partials/AppMeta.jsx
@@ -11,18 +11,18 @@ export default function AppMeta({ tenantDetails }) {
 
   const micrositeConfig = {
     1: {
+      applicationName: "discovery_hospitality",
       iconPrefix: "dh",
       themeColor: "#68192F",
     },
     2: {
+      applicationName: "discovery_suites",
       iconPrefix: "ds",
       themeColor: "#8FA6AD",
     },
   };
-
   const { iconPrefix, themeColor } = micrositeConfig[micrositeId] || {};
 
-  const ApplicationName = tenantDetails?.site_name || "";
   const AppleTouchIcon = iconPrefix ? iconPath(iconPrefix, 512) : "";
   const Icon192 = iconPrefix ? iconPath(iconPrefix, 192) : "";
   const Icon256 = iconPrefix ? iconPath(iconPrefix, 256) : "";
@@ -32,7 +32,13 @@ export default function AppMeta({ tenantDetails }) {
     <Head>
       <meta name="apple-mobile-web-app-capable" content="yes" />
       <meta name="apple-mobile-web-app-status-bar-style" content={themeColor} />
-      <meta name="apple-mobile-web-app-title" content={ApplicationName} />
+      <meta
+        name="apple-mobile-web-app-title"
+        content={
+          micrositeConfig?.[process.env.NEXT_PUBLIC_MICROSITE_ID]
+            ?.applicationName
+        }
+      />
       <link rel="apple-touch-icon" href={AppleTouchIcon} />
       <link rel="apple-touch-icon" sizes="512x512" href={AppleTouchIcon} />
       <link rel="apple-touch-icon" sizes="384x384" href={Icon384} />
