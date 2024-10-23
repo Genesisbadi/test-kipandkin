4ec6efcacb5ea9d000eda1ef318e889825116657
localnetwork <diomenikep@gmail.com>
2024-10-23 17:00:06 +0800

    chore: test pwa cache disable

--- next.config.js
+++ next.config.js
@@ -4,14 +4,28 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
 
 const runtimeCaching = require("next-pwa/cache");
 
-// Configuration object tells the next-pwa plugin
+// Add custom runtime caching to exclude specific URLs
+const customRuntimeCaching = [
+  ...runtimeCaching,
+  {
+    // Exclude static.triptease.io from caching
+    urlPattern: /^https:\/\/static\.triptease\.io\/.*/,
+    handler: "NetworkOnly",
+  },
+  {
+    // Exclude onboard.triptease.io from caching
+    urlPattern: /^https:\/\/onboard\.triptease\.io\/.*/,
+    handler: "NetworkOnly",
+  },
+];
+
 const withPWA = require("next-pwa")({
   dest: "public",
   sw: "service-worker.js",
   register: true,
   disable: process.env.NODE_ENV === "development",
   skipWaiting: true,
-  runtimeCaching,
+  runtimeCaching: customRuntimeCaching, // Use the custom runtime caching
   buildExcludes: [/middleware-manifest.json$/],
 });
 
@@ -20,7 +34,6 @@ const nextConfig = {
   reactStrictMode: false,
   images: {
     loader: "custom",
-    // loaderFile: "./components/partials/ImageLoader.jsx",
     loaderFile: "./lib/utils/cloudfrontLoader.jsx",
     remotePatterns: [
       {
@@ -47,82 +60,6 @@ const nextConfig = {
         pathname: `/vi/**`,
       },
     ],
-  },
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "X-Frame-Options",
-            value: "DENY", // This prevents the site from being framed
-          },
-          {
-            key: "Permissions-Policy",
-            value: "geolocation=(), microphone=()", // Customize the permissions as needed
-          },
-          {
-            key: "Content-Security-Policy",
-            value: `
-              default-src 'self';
-              script-src 'self' 'unsafe-eval' 'unsafe-inline' https://www.discoveryhotels-resorts.com/;
-              style-src 'self' 'unsafe-inline' https://www.discoveryhotels-resorts.com/;
-              img-src 'self' data:;
-              connect-src 'self' https://discoverysuites.haspcms.net;
-              font-src 'self' https://fonts.gstatic.com;
-              object-src 'none';
-              frame-ancestors 'none';
-              base-uri 'self';
-              form-action 'self';
-              upgrade-insecure-requests;
-            `
-              .replace(/\s{2,}/g, " ")
-              .trim(),
-          },
-          {
-            key: "Strict-Transport-Security",
-            value: "max-age=63072000; includeSubDomains; preload", // Customize as per your requirement
-          },
-          {
-            key: "X-Robots-Tag",
-            value: "",
-          },
-          {
-            key: "Cache-control",
-            value: "public, max-age=31536000, immutable",
-          },
-          {
-            key: "Expires",
-            value: new Date(Date.now() + 31536000000).toUTCString(),
-          },
-          {
-            key: "Content-Security-Policy",
-            value:
-              "script-src-attr 'self' 'unsafe-inline'; style-src-attr 'self' 'unsafe-inline'; frame-ancestors 'self'",
-          },
-          {
-            key: "Referrer-Policy",
-            value: "strict-origin-when-cross-origin",
-          },
-          {
-            key: "Strict-Transport-Security",
-            value: "max-age=63072000; includeSubDomains; preload",
-          },
-          {
-            key: "Permissions-Policy",
-            value: "geolocation=self",
-          },
-          {
-            key: "X-xss-protection",
-            value: "1; mode=block",
-          },
-          {
-            key: "X-Content-Type-Options",
-            value: "nosniff",
-          },
-        ],
-      },
-    ];
   },
 };
 
