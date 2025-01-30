64aebad0543ce611bd3474ecc91ad97f1b059c7e
localnetwork <diomenikep@gmail.com>
2025-01-30 14:24:59 +0800

    refactor: destination fields changed :<

--- components/blocks/Destination.jsx
+++ components/blocks/Destination.jsx
@@ -2,19 +2,20 @@ import Image from "next/image";
 import Link from "next/link";
 
 export default function OurCollection({ block, mediaHandler }) {
-  const collection = block?.main?.collection;
+  const destinations = block?.main?.destination;
   return (
     <>
       <section className="w-full">
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
-          {collection.contents.map((item, index) => (
+          {destinations.map((item, index) => (
             <div key={index} className="flex relative">
               <span className="absolute h-full w-full top-0 left-0 bg-[#000] opacity-[.3] z-[1]"></span>
               <Image
                 src={
-                  item?.mediaHandler["main.thumbnail"]?.[0].conversions
-                    .thumbnail ||
-                  item?.mediaHandler["main.thumbnail"]?.[0].original
+                  mediaHandler[`main.destination.${index}.image_icon`]?.[0]
+                    .conversions.thumbnail ||
+                  mediaHandler[`main.destination.${index}.image_icon`]?.[0]
+                    .original
                 }
                 alt={item?.title}
                 height={1000}
@@ -27,21 +28,17 @@ export default function OurCollection({ block, mediaHandler }) {
                   {item?.title}
                 </h2>
                 <div className="flex flex-col xl:flex-row gap-3 gap-x-5 px-3 w-full justify-center">
-                  {item?.data.main.book_now_button_link && (
+                  {item?.url_link && (
                     <Link
-                      target="_blank"
-                      href={item?.data.main.book_now_button_link}
-                      className={`w-full h-full 2sm:w-auto text-center text-white text-sm border-[1px] border-primary bg-primary hover:bg-secondary hover:border-secondary py-[20px] px-[30px] uppercase`}
+                      href={item?.url_link}
+                      target={
+                        item?.url_link?.startsWith("http") ? "_blank" : "_self"
+                      }
+                      className={`w-full h-full tracking-[1px] 2sm:w-auto text-center text-[14px] border border-white py-[20px] px-[30px] text-secondary lg:text-white bg-[#fff] bg-opacity-80 lg:bg-transparent hover:bg-[#fff] hover:text-secondary uppercase`}
                     >
-                      Book Now
+                      More Details
                     </Link>
                   )}
-                  <Link
-                    href={item?.route_url}
-                    className={`w-full h-full tracking-[1px] 2sm:w-auto text-center text-[14px] border border-white py-[20px] px-[30px] text-secondary lg:text-white bg-[#fff] bg-opacity-80 lg:bg-transparent hover:bg-[#fff] hover:text-secondary uppercase`}
-                  >
-                    More Details
-                  </Link>
                 </div>
               </div>
             </div>
