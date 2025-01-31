264134b7c9bf2d89717240184fb7f3b5940dd0e9
localnetwork <diomenikep@gmail.com>
2025-01-31 11:01:27 +0800

    chore: hide empty link

--- components/blocks/HeroGridRepeater.jsx
+++ components/blocks/HeroGridRepeater.jsx
@@ -151,21 +151,26 @@ export default function HeroGridRepeater({ block, mediaHandler }) {
                       }}
                     />
                   )}
-                  <div className="text-center">
-                    <Link
-                      className={`border-secondary hover:text-white text-[14px] uppercase inline-block border py-[15px] px-[30px] transition ${
-                        process.env.NEXT_PUBLIC_TEMPLATE == 2
-                          ? " hover:bg-primary hover:border-primary"
-                          : " hover:bg-secondary"
-                      }`}
-                      href={item.link || "#"}
-                      target={item.link.includes("http:") ? "_blank" : "_self"}
-                    >
-                      {item?.button_label
-                        ? item?.button_label
-                        : "Discover More"}
-                    </Link>
-                  </div>
+
+                  {item?.link && (
+                    <div className="text-center">
+                      <Link
+                        className={`border-secondary hover:text-white text-[14px] uppercase inline-block border py-[15px] px-[30px] transition ${
+                          process.env.NEXT_PUBLIC_TEMPLATE == 2
+                            ? " hover:bg-primary hover:border-primary"
+                            : " hover:bg-secondary"
+                        }`}
+                        href={item.link || "#"}
+                        target={
+                          item.link.includes("http:") ? "_blank" : "_self"
+                        }
+                      >
+                        {item?.button_label
+                          ? item?.button_label
+                          : "Discover More"}
+                      </Link>
+                    </div>
+                  )}
                 </div>
               </SectionAccordion>
             ))}
