fc572ee95530521a59102ec2c39e4895b5bf6ccf
localnetwork <diomenikep@gmail.com>
2025-02-10 15:56:30 +0800

    chore: updates

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 });
-const withCss = require("@zeit/next-css");
 const withPurgeCss = require("next-purgecss");
 const runtimeCaching = require("next-pwa/cache");
 
